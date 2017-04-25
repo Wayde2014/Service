fdfe94ddaf083f0d44364b334aca551d91b71e6f
Wayde <lynnheavn@126.com>
2017-04-25 23:31:43 +0800

    add register

--- application/common.php
+++ application/common.php
@@ -10,3 +10,30 @@
 // +----------------------------------------------------------------------
 
 // 应用公共文件
+
+/**
+ * 验证手机号码正确性
+ * @param $mobile
+ * @return bool
+ */
+function check_mobile($mobile)
+{
+    if (!is_numeric($mobile)) {
+        return false;
+    }
+    return preg_match('#^13[\d]{9}$|^14[5,7]{1}\d{8}$|^15[^4]{1}\d{8}$|^17[0,6,7,8]{1}\d{8}$|^18[\d]{9}$#', $mobile) ? true : false;
+}
+
+/**
+ * 检测身份证号码是否合法
+ * @param $idcode
+ * @return bool
+ */
+function check_idcode($idcode)
+{
+    if (preg_match('/^[0-9a-zA-Z]{15,18}$/D', $idcode)) {
+        return true;
+    } else {
+        return false;
+    }
+}
