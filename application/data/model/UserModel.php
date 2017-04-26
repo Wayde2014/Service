adc800e56fff92dcbacb3396bdbbe1a0ba2b78a2
Wayde <lynnheavn@126.com>
2017-04-26 22:44:47 +0800

    更新注册登录功能

--- application/data/model/UserModel.php
+++ application/data/model/UserModel.php
@@ -20,7 +20,8 @@ class UserModel extends Model
      */
     public function checkMobile($mobile)
     {
-        $ret = Db::name('user_info')->field('f_uid as uid')->where('f_mobile', $mobile)->select();
+        $table_name = 'user_info';
+        $ret = Db::name($table_name)->field('f_uid as uid')->where('f_mobile', $mobile)->select();
         if (empty($ret)) {
             return false;
         }
@@ -35,12 +36,13 @@ class UserModel extends Model
      */
     public function addUser($mobile, $lastdevice)
     {
+        $table_name = 'user_info';
         $data = array(
             'f_mobile' => $mobile,
             'f_lastdevice' => $lastdevice,
             'f_regtime' => date("Y-m-d H:i:s"),
         );
-        $userId = intval(Db::name('user')->insertGetId($data));
+        $userId = intval(Db::name($table_name)->insertGetId($data));
         if ($userId <= 0) {
             return false;
         }
@@ -55,7 +57,8 @@ class UserModel extends Model
      */
     public function checkSmslog($uid, $mobile)
     {
-        $ret = Db::name('user_smslog')->where('f_uid', $uid)
+        $table_name = 'user_smslog';
+        $ret = Db::name($table_name)->where('f_uid', $uid)
             ->where('f_mobile', $mobile)
             ->field('f_lasttime as lasttime')
             ->field('now() as curtime')
@@ -65,7 +68,7 @@ class UserModel extends Model
                 'f_uid' => $uid,
                 'f_mobile' => $mobile,
             );
-            Db::name('user_smslog')->insert($data);
+            Db::name($table_name)->insert($data);
         } else {
             $lasttime = $ret[0]['lasttime'];
             $curtime = $ret[0]['curtime'];
@@ -84,7 +87,8 @@ class UserModel extends Model
      */
     public function updateSmslog($uid, $mobile)
     {
-        $ret = Db::name('user_smslog')
+        $table_name = 'user_smslog';
+        $ret = Db::name($table_name)
             ->where('f_uid', $uid)
             ->where('f_mobile', $mobile)
             ->setInc('f_count');
@@ -92,6 +96,59 @@ class UserModel extends Model
             return true;
         }else{
             return false;
+        }
+    }
+
+
+    /**
+     * 记录登录信息
+     * @param $ck
+     * @param $uid
+     * @param $deviceid
+     * @param $platform
+     * @param $ip
+     * @param $remark
+     * @param $expiretime
+     * @return array
+     */
+    public function addUserLogin($ck, $uid, $deviceid, $platform, $ip, $remark, $expiretime){
+        $table_name = 'user_login';
+        //判断用户当前登录态是否已失效
+        $ret = Db::name($table_name)
+            ->where('f_uid', $uid)
+            ->where('f_expiretime', '> time', time())
+            ->field('f_usercheck as usercheck')
+            ->select();
+
+        if(!empty($ret)){
+            //更新过期时间
+            $usercheck = $ret[0]['usercheck'];
+            $data = array(
+                'f_expiretime' => $expiretime,
+            );
+            Db::name($table_name)->where('f_usercheck',$usercheck)->update($data);
+            return array(
+                'usercheck' => $usercheck,
+                'expiretime' => $expiretime,
+            );
+        }else{
+            $data = array(
+                'f_usercheck' => $ck,
+                'f_uid' => $uid,
+                'f_deviceid' => $deviceid,
+                'f_platform' => $platform,
+                'f_ip' => $ip,
+                'f_remark' => $remark,
+                'f_expiretime' => $expiretime,
+            );
+            Db::name($table_name)->insert($data);
+            if(Db::name($table_name)->getLastInsID() <= 0){
+                return false;
+            }
+            return array(
+                'usercheck' => $ck,
+                'expiretime' => $expiretime,
+            );
         }
     }
 }
--- doc/数据库设计.sql
+++ doc/数据库设计.sql
@@ -84,19 +84,16 @@ CREATE TABLE `t_user_draw_order` (
 /*用户管理-登录信息表*/
 CREATE TABLE `t_user_login` (
   `f_id` int NOT NULL AUTO_INCREMENT COMMENT '自增长ID',
-  `f_uid` int NOT NULL COMMENT '用户ID',
-  `f_usercheck` varchar(100) NOT NULL COMMENT '登录时的ck',
-  `f_logname` varchar(100) DEFAULT NULL COMMENT '登录时的name',
-  `f_uuid` varchar(100) NOT NULL COMMENT 'uuid,设备号',
-  `f_platform` varchar(50) DEFAULT NULL COMMENT '平台：1 web主站,2 android, 3 IOS, 4 H5',
+  `f_usercheck` varchar(200) NOT NULL COMMENT '登录ck',
+  `f_uid` int NOT NULL COMMENT '登录用户ID',
+  `f_deviceid` varchar(200) DEFAULT NULL COMMENT '登录设备号',
+  `f_platform` tinyint DEFAULT 1 COMMENT '平台：1 web主站,2 android, 3 IOS, 4 H5',
   `f_ip` varchar(50) DEFAULT NULL COMMENT '登录ip',
   `f_remark` varchar(500) DEFAULT NULL COMMENT '附属信息',
-  `f_status` int NOT NULL COMMENT '状态(1 成功 0 失败)',
-  `f_note` varchar(500) DEFAULT NULL COMMENT '备注',
+  `f_expiretime` timestamp not null comment '登录过期时间(默认30天后过期)',
   `f_lasttime` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP COMMENT '更新时间',
   PRIMARY KEY (`f_id`),
-  KEY `idx_uid` (`f_uid`),
-  KEY `idx_ip` (`f_ip`)
+  unique key (`f_usercheck`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 comment '用户管理-登录信息表';
 
 
