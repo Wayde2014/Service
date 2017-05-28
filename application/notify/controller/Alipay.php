9c2815c61e854a7012eba4ccee851ae1b27ee49b
Wayde <lynnheavn@126.com>
2017-05-28 18:11:46 +0800

    支付宝退款通知

--- application/notify/controller/Alipay.php
+++ application/notify/controller/Alipay.php
@@ -59,45 +59,91 @@ class Alipay
             if(array_key_exists('body',$notify_args)){
                 $describle = $notify_args['body'];
             }
+            $refund_fee = '';   //总退款金额
+            if(array_key_exists('refund_fee',$notify_args)){
+                $refund_fee = $notify_args['refund_fee'];
+            }
+            $gmt_refund = '';   //交易退款时间
+            if(array_key_exists('gmt_refund',$notify_args)){
+                $gmt_refund = $notify_args['gmt_refund'];
+            }
+            $out_biz_no = '';   //退款订单号
+            if(array_key_exists('out_biz_no',$notify_args)){
+                $out_biz_no = $notify_args['out_biz_no'];
+            }
 
             //验证此次通知内容是否有效
             if($app_id != $AlipayBase->appid || $seller_id != $AlipayBase->uuid){
                 exception("无效通知");
             }
-            if(!in_array($trade_status,array('TRADE_SUCCESS','TRADE_FINISHED'))){
+            if(!in_array($trade_status,array('TRADE_SUCCESS','TRADE_FINISHED','TRADE_CLOSED'))){
                 exception("暂不处理的通知交易状态");
             }
 
-            //处理支付通知
-            $Account = new Account();
-            $orderid = $out_trade_no;
-            $orderinfo = $Account->getRechargeOrderInfo($orderid);
-            if(empty($orderinfo)){
-                exception('该笔充值订单不存在');
-            }
-            $channel = intval($orderinfo['channel']);
-            if($channel != config("paychannel.alipay")){
-                exception('该笔充值订单非支付宝渠道');
-            }
-            $order_status = intval($orderinfo['status']);
-            if($order_status != 0){
-                Log::record("充值订单[".$orderid."]已处理完成,当前状态-".$order_status,'info');
-                exit("success");
-            }
-            $order_money = number_format($orderinfo['paymoney'],2,'.','');
-            $bankmoney = number_format($total_amount,2,'.','');
-            if($order_money != $bankmoney){
-                exception("充值订单[".$orderid."]网站金额[".$order_money."]与支付宝金额[".$bankmoney."]不一致");
+            if(in_array($trade_status,array('TRADE_SUCCESS','TRADE_FINISHED'))){
+                //处理支付通知
+                $Account = new Account();
+                $orderid = $out_trade_no;
+                $orderinfo = $Account->getRechargeOrderInfo($orderid);
+                if(empty($orderinfo)){
+                    exception('该笔充值订单不存在');
+                }
+                $channel = intval($orderinfo['channel']);
+                if($channel != config("paychannel.alipay")){
+                    exception('该笔充值订单非支付宝渠道');
+                }
+                $order_status = intval($orderinfo['status']);
+                if($order_status != 0){
+                    Log::record("充值订单[".$orderid."]已处理完成,当前状态-".$order_status,'info');
+                    exit("success");
+                }
+                $order_money = number_format($orderinfo['paymoney'],2,'.','');
+                $bankmoney = number_format($total_amount,2,'.','');
+                if($order_money != $bankmoney){
+                    exception("充值订单[".$orderid."]网站金额[".$order_money."]与支付宝金额[".$bankmoney."]不一致");
+                }
+
+                //充值成功，入账处理
+                $result = $Account->rechargeSuc($orderid,$trade_no,$bankmoney,$buyer_logon_id,$describle);
+                if(!$result){
+                    exception("充值订单[".$orderid."]入账失败");
+                }else{
+                    Log::record("充值订单[".$orderid."]入账处理成功",'info');
+                    exit("success");
+                }
+            }elseif(in_array($trade_status,array('TRADE_CLOSED'))){
+                //处理退款通知
+                $Account = new Account();
+                $orderid = $out_biz_no;
+                $orderinfo = $Account->getDrawOrderInfo($orderid);
+                if(empty($orderinfo)){
+                    exception('该笔退款订单不存在');
+                }
+                $channel = intval($orderinfo['channel']);
+                if($channel != config("drawchannel.alipay")){
+                    exception('该笔退款订单非支付宝渠道');
+                }
+                $order_status = intval($orderinfo['status']);
+                if($order_status != 0){
+                    Log::record("退款订单[".$orderid."]已处理完成,当前状态-".$order_status,'info');
+                    exit("success");
+                }
+                $order_money = number_format($orderinfo['drawmoney'],2,'.','');
+                $bankmoney = number_format($total_amount,2,'.','');
+                if($order_money != $bankmoney){
+                    exception("退款订单[".$orderid."]网站金额[".$order_money."]与支付宝金额[".$bankmoney."]不一致");
+                }
+
+                //退款成功，入账处理
+                $result = $Account->drawSuc($orderid,$channel,$trade_no,$bankmoney,$buyer_logon_id,$describle);
+                if(!$result){
+                    exception("退款订单[".$orderid."]入账失败");
+                }else{
+                    Log::record("退款订单[".$orderid."]入账处理成功",'info');
+                    exit("success");
+                }
             }
 
-            //充值成功，入账处理
-            $result = $Account->rechargeSuc($orderid,$trade_no,$bankmoney,$buyer_logon_id,$describle);
-            if(!$result){
-                exception("充值订单[".$orderid."]入账失败");
-            }else{
-                Log::record("充值订单[".$orderid."]入账处理成功",'info');
-                exit("success");
-            }
         }catch (Exception $e){
             Log::record($e,'error');
             exit('fail');
