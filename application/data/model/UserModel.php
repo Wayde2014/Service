57abf337f446da88a490c29d26fd57f2ba57cfd9
Wayde <lynnheavn@126.com>
2017-06-11 22:30:42 +0800

    收货地址列表排序修改

--- application/data/model/UserModel.php
+++ application/data/model/UserModel.php
@@ -325,6 +325,7 @@ class UserModel extends Model
         $address = Db::name($table_name)
             ->where('f_uid', $userid)
             ->field('f_id id,f_province province,f_city city,f_address address,f_name name,f_sex as male,f_mobile mobile,f_isactive isactive')
+            ->order('f_isactive', 'desc')
             ->order('f_addtime', 'desc')
             ->select();
         return $address;
